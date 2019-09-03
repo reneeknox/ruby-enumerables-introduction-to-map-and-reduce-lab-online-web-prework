fa1cce59ba0f96fb47deae3e3391af47530dde2d
Renee Knox <renee5.0@outlook.com>
2019-09-03 15:09:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,14 @@ def map_to_negativize(array)
   end
   final_array
 end
+
+def map_to_negativize(array)
+  final_array = []
+  counter = 0
+  while counter < array.size do
+  #  DO SOMETHING< PUSH IN THE FINAL_ARRAY THE CURRENT ELEMENT TIMES -1
+    final_array.push(array[counter] * -1)
+    counter += 1
+  end
+  final_array
+end

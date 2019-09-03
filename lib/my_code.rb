dbf247d86b4778851ad4e0efce194e8c8928aabe
Renee Knox <renee5.0@outlook.com>
2019-09-03 15:06:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,7 +4,7 @@ def map_to_negativize(array)
   counter = 0
   while counter < array.size do
   #  DO SOMETHING< PUSH IN THE FINAL_ARRAY THE CURRENT ELEMENT TIMES -1
-    final_array.push.array[]
+    final_array.push(array[counter])
     counter += 1
   end
   final_array
